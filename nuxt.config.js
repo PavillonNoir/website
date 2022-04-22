3efb25e40b3e12c590b09442fa2b1ca8fdc1bce6
mechack tambwe <gustave@pavillonnoir.com>
2022-04-22 17:05:00 +0200

    add cache modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,6 +78,7 @@ export default {
         /* other options of WP-API */
       },
     ],
+    'nuxt-ssr-cache',
   ],
 
   bootstrapVue: {
@@ -89,6 +90,40 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.API_URL,
   },
+  cache: {
+    // if you're serving multiple host names (with differing
+    // results) from the same server, set this option to true.
+    // (cache keys will be prefixed by your host name)
+    // if your server is behind a reverse-proxy, please use
+    // express or whatever else that uses 'X-Forwarded-Host'
+    // header field to provide req.hostname (actual host name)
+    useHostPrefix: false,
+    pages: [
+      // these are prefixes of pages that need to be cached
+      // if you want to cache all pages, just include '/'
+      '/agency',
+      '/projects',
+ 
+      // you can also pass a regular expression to test a path
+      /^\/page3\/\d+$/,
+ 
+      // to cache only root route, use a regular expression
+      /^\/$/
+    ],
+
+    store: {
+      type: 'memory',
+ 
+      // maximum number of pages to store in memory
+      // if limit is reached, least recently used page
+      // is removed.
+      max: 100,
+ 
+      // number of seconds to store this page in cache
+      ttl: 60,
+    },
+  },
+ 
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
